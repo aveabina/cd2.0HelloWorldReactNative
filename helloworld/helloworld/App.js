75e713c16a3713e08fe99db62f7c67d3ee737ccc
aveabina <91433112+aveabina@users.noreply.github.com>
2022-03-25 10:26:32 +0300

    Add files via upload

---
+++ helloworld/helloworld/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const MyApp = () => {
+  return (
+    <View style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
+      
+      <Text >
+        Hello ReactNative and Expo!
+      </Text>
+      
+     
+      
+    </View>
+  );
+}
+
+export default MyApp;
